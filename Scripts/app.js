04d95b3c93eb0b1702887fee0b9219ebb693c5da
ChadClark_Dev <chadclarke@bell.net>
2016-04-21 11:40:00 -0400

    last update working correct

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,17 +1,18 @@
 // ROLLING THE DICE 
 // declaring all of the dice images and their src to be pulled in 
-var face0 = new Image()
-face0.src = "Content/img/dice1.png"
+
 var face1 = new Image()
-face1.src = "Content/img/dice2.png"
+face1.src = "Content/img/dice1.png"
 var face2 = new Image()
-face2.src = "Content/img/dice3.png"
+face2.src = "Content/img/dice2.png"
 var face3 = new Image()
-face3.src = "Content/img/dice4.png"
+face3.src = "Content/img/dice3.png"
 var face4 = new Image()
-face4.src = "Content/img/dice5.png"
+face4.src = "Content/img/dice4.png"
 var face5 = new Image()
-face5.src = "Content/img/dice6.png"
+face5.src = "Content/img/dice5.png"
+var face6 = new Image()
+face6.src = "Content/img/dice6.png"
 
 // The function to roll the dice
 function TossTheDice() {
