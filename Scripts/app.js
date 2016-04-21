0bfd21879ee4ac4066b50ef3550e2d93dbd23223
ChadClark_Dev <chadclarke@bell.net>
2016-04-21 10:38:30 -0400

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,34 @@
+// declaring all of the dice images and their src to be pulled in 
+var face0=new Image()
+face0.src="Content/img/dice1.png"
+var face1=new Image()
+face1.src="Content/img/dice2.png"
+var face2=new Image()
+face2.src="Content/img/dice3.png"
+var face3=new Image()
+face3.src="Content/img/dice4.png"
+var face4=new Image()
+face4.src="Content/img/dice5.png"
+var face5=new Image()
+face5.src="Content/img/dice6.png"
+
+// The function to roll the dice
+
+ function TossTheDice(){
+    //Dice #1     
+        //create a random integer between 0 and 6
+        var randomdice=Math.round(Math.random()*6)
+        //pull image from the src 
+        document.images["rolldice"].src=eval("face"+randomdice+".src")
+        console.log(randomdice);
+    //Dice #2
+        //create a random integer between 0 and 6 and offset this dice roll by adding one 
+        var randomdice=Math.round(Math.random()*6+1)
+        //pull image from the src 
+        document.images["rolldice2"].src=eval("face"+randomdice+".src")
+      
+
+    }
+    
+
+
